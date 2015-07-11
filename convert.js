ced678b8d2de12b7264bf0dad899aed7826390ee
daniel <dspinoz@gmail.com>
2015-07-11 10:38:57 +0930

    convert whale

--- convert.js
+++ convert.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 var svg2png = require('svg2png');
 
-svg2png("whale.svg", "bird.png", function (err) {
+svg2png("whale.svg", "whale.png", function (err) {
     if (err) { console.log(err); return; }
     // PNGs for everyone!
 });
