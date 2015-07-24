0c974291fd439498c0c9de3b8a33478f14a12f1d
daniel <dspinoz@gmail.com>
2015-07-24 17:42:59 +0930

    not server side

--- convert.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-var svg2png = require('svg2png');
-
-svg2png("whale.svg", "whale.png", function (err) {
-    if (err) { console.log(err); return; }
-    // PNGs for everyone!
-});
-
