07baacc616f972b3782266717906f869fc363cc5
Daniel <dspinoz@gmail.com>
2015-08-26 10:54:37 +0930

    make calendar view reusable

--- public/js/kindle.js
+++ public/js/kindle.js
@@ -1,132 +1,139 @@
 
 
-var ymdFormat = d3.time.format('%Y-%m-%d'),
-    timeFormat = d3.time.format('%-I:%M %p'),
+var timeFormat = d3.time.format('%-I:%M %p'),
     monthFormat = d3.time.format('%B'),
     dayNumFormat = d3.time.format('%e'),
     dayNameFormat = d3.time.format('%A'),
     yearFormat = d3.time.format('%Y');
 
-var chart = new d3Kit.Skeleton('#month-view', {
+var calendarChart = d3Kit.factory.createChart(
+  {
     margin: {top: 0, right: 0, bottom: 0, left: 0}
-  })
-  .autoResize('width')
-  .autoResizeToAspectRatio(1);
-  
-var redraw = function(date) {
-
-  if (Object.prototype.toString.call( date ) !== '[object Date]') {
-    // not a date - chart size (from resize)
-    return;
-  }
-  
-  if (!chart.hasData()) {
-    return;
-  }
-  
-  var x = chart.getInnerWidth()/7;
-  var y = chart.getInnerHeight()/7;
-  var r = x/2;
-
-  var g = chart.getRootG().selectAll('g')
-    .data(function() {
-      var start = d3.time.sunday.floor(date);
-      
-      while (date.getMonth() == start.getMonth() && 
-             start.getDate() != 0) {
-        start = d3.time.sunday.floor(new Date(start.getTime() - (24*60*60*1000)));
-      }
-      
-      var end = d3.time.sunday.ceil(new Date(date.getFullYear(), date.getMonth()+1, 0));
-      
-      var days = d3.time.days(start, end);
-
-      while (days.length < 42) {
-        end = d3.time.sunday.ceil(new Date(end.getTime() + (24*60*60*1000)));
-        days = d3.time.days(start, end);
-      }
-          
-      return [days.slice(0, 7),
-          days.slice(7, 14),
-          days.slice(14, 21),
-          days.slice(21, 28),
-          days.slice(28, 35),
-          days.slice(35, 42)];
+  }, 
+  [],
+  function(skel) {
+    
+    skel.mixin({
+      'dayhover': function(d,i) {
+      },
+      'ymdFormat': d3.time.format('%Y-%m-%d'),
+      'monthFormat': d3.time.format('%B'),
+      'dayNumFormat': d3.time.format('%e')
     });
     
-  g.exit().remove();
-  
-  g.enter()
-    .append('g');
-  
-  g.attr('transform', function(d,i) {
-    return 'translate('+r+', ' + i*y + ')';
-  });
-
-  var c = g.selectAll('circle.cal')
-    .data(function(d) { return d; });
+    var resize = function(dim) {
+      redraw(skel.data());
+    };
     
-  c.exit().remove();
-   
-  c.enter()
-      .append('circle')
-      .classed('cal', true);
+    var redraw = function() {
+    
+      if (!skel.hasData()) {
+        return;
+      }
       
-  c.classed('current', function(d) {
-    if (ymdFormat(date) == ymdFormat(d)) {
-      return true;
-    }
-    return false;
-  })
-  .classed('in-month', function(d) {
-    if (monthFormat(date) == monthFormat(d)) {
-      return true;
-    }
-    return false;
-  })
-  .attr('cx', function(d,i) { return i*x; })
-  .attr('cy', r)
-  .attr('r', r)
-  .on('mouseover', function(d) {
-    console.log(d);
-    d3.select('#circle-data').text(JSON.stringify(d));
-  });
+      var date = skel.data();
+      
+      var x = skel.getInnerWidth()/7;
+      var y = x;
+      var r = x/2;
 
-  var txt = g.selectAll('text.cal').data(function(d) { return d; });
-  
-  txt.exit().remove();
-  
-  txt.enter()
-    .append('text')
-    .classed('cal', true);
+      var g = skel.getRootG().selectAll('g')
+        .data(function() {
+          var start = d3.time.sunday.floor(date);
+          
+          while (date.getMonth() == start.getMonth() && 
+                 start.getDate() != 0) {
+            start = d3.time.sunday.floor(new Date(start.getTime() - (24*60*60*1000)));
+          }
+          
+          var end = d3.time.sunday.ceil(new Date(date.getFullYear(), date.getMonth()+1, 0));
+          
+          var days = d3.time.days(start, end);
+
+          while (days.length < 42) {
+            end = d3.time.sunday.ceil(new Date(end.getTime() + (24*60*60*1000)));
+            days = d3.time.days(start, end);
+          }
+              
+          return [days.slice(0, 7),
+              days.slice(7, 14),
+              days.slice(14, 21),
+              days.slice(21, 28),
+              days.slice(28, 35),
+              days.slice(35, 42)];
+        });
         
-  txt.classed('current', function(d) {
-    if (ymdFormat(date) == ymdFormat(d)) {
-      return true;
-    }
-    return false;
-  })
-  .classed('in-month', function(d) {
-    if (monthFormat(date) == monthFormat(d)) {
-      return true;
-    }
-    return false;
-  })
-  .attr('x', function(d,i) { return i*x; })
-  .attr('y', r+(r/4))
-  .attr('font-size', r)
-  .attr('text-anchor', 'middle')
-  .text(function(d) {
-    return dayNumFormat(d);
+      g.exit().remove();
+      
+      g.enter()
+        .append('g');
+      
+      g.attr('transform', function(d,i) {
+        return 'translate('+r+', ' + i*y + ')';
+      });
+
+      var c = g.selectAll('circle.cal')
+        .data(function(d) { return d; });
+        
+      c.exit().remove();
+       
+      c.enter()
+          .append('circle')
+          .classed('cal', true);
+          
+      c.classed('current', function(d) {
+        if (skel.ymdFormat(date) == skel.ymdFormat(d)) {
+          return true;
+        }
+        return false;
+      })
+      .classed('in-month', function(d) {
+        if (skel.monthFormat(date) == skel.monthFormat(d)) {
+          return true;
+        }
+        return false;
+      })
+      .attr('cx', function(d,i) { return i*x; })
+      .attr('cy', r)
+      .attr('r', r)
+      .on('mouseover', function(d,i) {
+        skel.dayhover(d,i);
+      });
+
+      var txt = g.selectAll('text.cal').data(function(d) { return d; });
+      
+      txt.exit().remove();
+      
+      txt.enter()
+        .append('text')
+        .classed('cal', true);
+            
+      txt.classed('current', function(d) {
+        if (skel.ymdFormat(date) == skel.ymdFormat(d)) {
+          return true;
+        }
+        return false;
+      })
+      .classed('in-month', function(d) {
+        if (skel.monthFormat(date) == skel.monthFormat(d)) {
+          return true;
+        }
+        return false;
+      })
+      .attr('x', function(d,i) { return i*x; })
+      .attr('y', r+(r*0.3)) //get text into the centre of the circle
+      .attr('font-size', r)
+      .attr('text-anchor', 'middle')
+      .text(function(d) {
+        return skel.dayNumFormat(d);
+      });
+    };
+
+    skel
+      .autoResize('width')
+      .on('resize', resize)
+      .on('data', redraw);
   });
-}
-  
-chart
-  .on('resize', redraw)
-  .on('data', redraw);
-  
-
-
 
 var textSvg = d3Kit.factory.createChart(
   {
@@ -215,7 +222,9 @@ var textSvg = d3Kit.factory.createChart(
       .on('data', redraw);
   });
 
-
+  
+var calendar = new calendarChart('#month-view');
+  
 var time = new textSvg('#time').mixin({
   'text': function(d) {return timeFormat(d);}
 });
@@ -233,6 +242,7 @@ var dateLong = new textSvg('#date').mixin({
     return dayNumFormat(d)+' '+monthFormat(d);}
 });
 
+
 function updateDate() {
   
   var date = new Date();
@@ -242,7 +252,7 @@ function updateDate() {
   day.data(date);
   dateLong.data(date);
   
-  chart.data(date);
+  calendar.data(date);
   
   d3.select('#footer').html('<span class="fa fa-refresh"></span> Last refreshed at ' + d3.time.format.iso(new Date()));
 }
